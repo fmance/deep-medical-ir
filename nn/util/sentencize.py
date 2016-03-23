cf6882e4ffec0ea9a0309c179499c9c091402e1b
Felix Mance <felixmance@gmail.com>
2016-03-23 20:53:04 +0100

    Discard any non-letter symbols before feeding to Word2Vec or NN

--- nn/util/sentencize.py
+++ nn/util/sentencize.py
@@ -4,11 +4,12 @@ import nltk.data
 import os
 import codecs
 import time
+import regex
 
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 
 def stripAndLower(s):
-    return s.rstrip(".").lower()
+    return regex.sub(ur"\P{Latin}", " ", s).lower()
 
 def getSentences(text):
     sent = sent_detector.tokenize(text.strip(), realign_boundaries=True)
@@ -17,9 +18,9 @@ def getSentences(text):
 def sentencize():
     count = 0
     start = time.time()
-    sent_dir = "../data/sentences/"
+    sent_dir = "../../data/sentences/"
     os.mkdir(sent_dir)
-    for sdir in ["../data/plaintext/" + d for d in ["00", "01", "02", "03"]]:
+    for sdir in ["../../data/plaintext/" + d for d in ["00", "01", "02", "03"]]:
         sent_sdir = os.path.join(sent_dir, os.path.basename(sdir))
         os.mkdir(sent_sdir)
         for ssdir in [os.path.join(sdir, ssdir) for ssdir in sorted(os.listdir(sdir))]:
@@ -28,7 +29,7 @@ def sentencize():
             for fname in [os.path.join(ssdir, fname) for fname in sorted(os.listdir(ssdir))]:
                 count += 1
                 if count % 1000 == 0:
-                    print "Sentencized %d files (%s), took %02d minutes" % (count, ssdir, (time.time() - start)/60)
+                    print "Sentencized %d files (%s), took %.2f minutes" % (count, ssdir, (time.time() - start)/60.0)
                 fp = codecs.open(fname, "r", "utf-8")
                 out = codecs.open(os.path.join(sent_ssdir, os.path.basename(fname) + ".sent"), "w", "utf-8")
                 for sentence in getSentences(fp.read()):
