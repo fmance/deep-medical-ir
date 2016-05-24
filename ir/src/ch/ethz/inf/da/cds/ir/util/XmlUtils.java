252b7e22f3dc587a0ba4a605164d86cc31a53f40
Felix Mance <felixmance@gmail.com>
2016-05-24 21:57:06 +0200

    Change XML parsing - parse also section headings and clean queries

--- ir/src/ch/ethz/inf/da/cds/ir/util/XmlUtils.java
+++ ir/src/ch/ethz/inf/da/cds/ir/util/XmlUtils.java
@@ -16,13 +16,14 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import ch.ethz.inf.da.cds.ir.Article;
+import ch.ethz.inf.da.cds.ir.FilePaths;
 import ch.ethz.inf.da.cds.ir.TrecQuery;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
 
 public class XmlUtils {
-    public static Article parseArticle(File file) throws IOException, ParserConfigurationException {
+    public static Article parseArticle(File file) throws Exception {
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 
@@ -32,8 +33,8 @@ public class XmlUtils {
         try {
             doc = dBuilder.parse(file);
         } catch (SAXException | IOException e) {
-            throw new IOException(
-                    "Error parsing document " + file.getAbsolutePath() + " : " + e.getMessage(), e);
+            throw new IOException("Error parsing document " + file.getAbsolutePath() + " : " + e.getMessage(),
+                                  e);
         }
         doc.getDocumentElement().normalize();
 
@@ -43,14 +44,38 @@ public class XmlUtils {
             title = titleNodeList.item(0).getTextContent();
         }
 
-        StringBuilder text = new StringBuilder();
-        NodeList nodes = doc.getElementsByTagName("p");
-        for (int i = 0; i < nodes.getLength(); i++) {
-            text.append(nodes.item(i).getTextContent());
-            text.append("\n\n");
+        StringBuilder body = new StringBuilder();
+        NodeList abstractNodeList = doc.getElementsByTagName("abstract");
+        if (abstractNodeList.getLength() > 0) {
+            getTextContentRecursive(body, abstractNodeList.item(0));
         }
 
-        return new Article(pmcid, title, text.toString());
+        body.append("\n\n");
+
+        NodeList bodyNodeList = doc.getElementsByTagName("body");
+        if (bodyNodeList.getLength() > 0) {
+            getTextContentRecursive(body, bodyNodeList.item(0));
+        }
+
+        return new Article(pmcid,
+                           title,
+                           body.toString());
+    }
+
+    private static void getTextContentRecursive(StringBuilder sb, Node node) {
+        Node child = node.getFirstChild();
+        while (child != null) {
+            if (child.getNodeType() == Node.TEXT_NODE) {
+                String content = child.getTextContent();
+                if (content != null) {
+                    sb.append(' ');
+                    sb.append(content);
+                }
+            } else {
+                getTextContentRecursive(sb, child);
+            }
+            child = child.getNextSibling();
+        }
     }
 
     public static List<TrecQuery> parseQueries(File file) throws ParserConfigurationException, SAXException,
@@ -67,21 +92,65 @@ public class XmlUtils {
             Node node = nodes.item(i);
             NamedNodeMap attributes = node.getAttributes();
             int id = Integer.parseInt(attributes.getNamedItem("number").getTextContent());
-            TrecQuery.TYPE type = TrecQuery.TYPE.valueOf(attributes.getNamedItem("type").getTextContent()
-                    .toUpperCase());
+            TrecQuery.TYPE type = TrecQuery.TYPE.valueOf(attributes.getNamedItem("type")
+                                                                   .getTextContent()
+                                                                   .toUpperCase());
 
             NodeList children = node.getChildNodes();
             String description = children.item(1).getTextContent().trim();
             String summary = children.item(3).getTextContent().trim();
+
+            summary = summary.replaceAll("\\d+", " ");
+            summary = summary.replaceAll("\\byear\\b", " ");
+            summary = summary.replaceAll("\\bold\\b", " ");
+            summary = summary.replaceAll("\\byo\\b", " ");
+            summary = summary.replaceAll("\\bwho\\b", " ");
+            summary = summary.replaceAll("\\bwith\\b", " ");
+            summary = summary.replaceAll("\\bfrom\\b", " ");
+            summary = summary.replaceAll("\\bmale\\b", " ");
+            summary = summary.replaceAll("\\bfemale\\b", " ");
+            summary = summary.replaceAll("\\badult\\b", " ");
+            summary = summary.replaceAll("\\bleft\\b", " ");
+            summary = summary.replaceAll("\\bright\\b", " ");
+            summary = summary.replaceAll("\\bhe\\b", " ");
+            summary = summary.replaceAll("\\bshe\\b", " ");
+            summary = summary.replaceAll("\\bher\\b", " ");
+            summary = summary.replaceAll("\\bwhile\\b", " ");
+            summary = summary.replaceAll("\\bnormal\\b", " ");
+            summary = summary.replaceAll("\\bday\\w*\\b", " ");
+            summary = summary.replaceAll("\\bweek\\w*\\b", " ");
+            summary = summary.replaceAll("\\bmonth\\w*\\b", " ");
+            summary = summary.replaceAll("\\bboth\\b", " ");
+            summary = summary.replaceAll("\\bnow\\b", " ");
+
+            System.out.println(summary);
+
+            if (id <= 10) {
+                summary += " diagnosis diagnose";
+            }
+            if (id > 10 && id <= 20) {
+                summary += " routine test";
+            }
+            if (id > 20) {
+                summary += " therapy treat treatment";
+            }
 
             Optional<String> diagnosis = Optional.absent();
             if (children.getLength() > 5) {
                 diagnosis = Optional.of(children.item(5).getTextContent().trim());
             }
 
-            queries.add(new TrecQuery(id, type, description, summary, diagnosis));
+            queries.add(new TrecQuery(id,
+                                      type,
+                                      description,
+                                      summary,
+                                      diagnosis));
         }
 
         return queries;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(parseArticle(new File(FilePaths.PMC_00_DIR.toString() + "/00/2637234.nxml")));
     }
 }
