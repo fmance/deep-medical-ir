77f8bce98e8ab80f062b9d52b4f07825d39d249c
Felix Mance <felixmance@gmail.com>
2016-05-31 02:43:16 +0200

    Revert simple rerank per year individually

--- ranking/simplererank.py
+++ ranking/simplererank.py
@@ -7,27 +7,19 @@ import subprocess
 import numpy as np
 import itertools
 import sys
+import pprint
 
 sys.path.insert(0, "../utils/")
 import utils
 
 classifier = sys.argv[1]
-class_id = sys.argv[2]
-#resYear = sys.argv[3]
-#qrelYear = resYear[:4]
+year = sys.argv[2]
+class_id = sys.argv[3]
 
-qrelsSampleEval2014 = "../data/qrels/qrels-sampleval-2014.txt"
-qrelsSampleEval2015 = "../data/qrels/qrels-sampleval-2015.txt"
-
-baseline2014 = "../ir/results/results-2014-" + class_id + ".txt"
-baseline2015 = "../ir/results/results-2015-" + class_id + ".txt"
-
-reranked2014 = baseline2014 + ".reranked." + classifier
-reranked2015 = baseline2015 + ".reranked." + classifier
-
-#qrel_file = "../data/qrels/qrels-treceval-" + qrelYear + ".txt"
-#baseline_results_file = "../ir/results/results-" + resYear + "-" + class_id + ".txt"
-#reranked_results_file = baseline_results_file + ".reranked." + classifier
+qrelsSampleEval = "../data/qrels/qrels-sampleval-" + year + ".txt"
+qrelsTrecEval = "../data/qrels/qrels-treceval-" + year + ".txt"
+baselineResultsFile = "../ir/results/results-" + year + "-" + class_id + ".txt"
+rerankedFile = baselineResultsFile + ".reranked." + classifier
 
 eval_progname = "../eval/trec_eval.9.0/trec_eval"
 perl_progname = "../eval/sample_eval.pl"
@@ -45,8 +37,7 @@ def zscore_dict(d):
 
 class_predictions = utils.readClassPredictions(classifier, class_id)
 class_predictions = zscore_dict(class_predictions)
-baseline_results_2014 = utils.readResults(baseline2014)
-baseline_results_2015 = utils.readResults(baseline2015)
+baselineResults = utils.readResults(baselineResultsFile)
 
 topic_offset = topic_offsets[class_id]
 
@@ -57,8 +48,7 @@ def getNormScores(baselineResults):
 		normScores[topic_id] = dict(zip(doc, stats.zscore(scores)))
 	return normScores
 
-baselineNormScores2014 = getNormScores(baseline_results_2014)
-baselineNormScores2015 = getNormScores(baseline_results_2015)
+baselineNormScores = getNormScores(baselineResults)
 
 def simple_rerank(weight, baselineNormScores, rerankedFile):
 	reranked_results = open(rerankedFile, "w")
@@ -73,8 +63,7 @@ def simple_rerank(weight, baselineNormScores, rerankedFile):
 			rank += 1
 	reranked_results.close()
 
-def get_precision_at(prec_num, weight, qrelsFile, baselineNormScores, rerankedFile):
-	simple_rerank(weight, baselineNormScores, rerankedFile)
+def get_precision_at(prec_num, qrelsFile, rerankedFile):
 	output = subprocess.Popen([eval_progname, qrelsFile, rerankedFile], stdout=subprocess.PIPE).communicate()[0]
 	output = output.split()
 	index = output.index("P_" + str(prec_num))
@@ -87,40 +76,35 @@ def get_infNDCG(weight, qrelsSampleEvalFile, baselineNormScores, rerankedFile):
 	index = output.index("infNDCG")
 	return float(output[index + 2])
 
-def get_best_weight(measure):
-	best_weight = 0.0
-	measure_max = 0.0
-	measure_max_2014 = 0.0
-	measure_max_2015 = 0.0
-	all_res = []
-	for weight in np.linspace(0,1,21):
+def getBestWeights(measure, qrelsTrecEval, qrelsSampleEval, baselineNormScores, rerankedResultsFile):
+	weightMap = []
+	maxVal = 0.0
+	for weight in np.linspace(0.2,1,41):
 		if measure == "infNDCG":
-			measure2014 = get_infNDCG(weight, qrelsSampleEval2014, baselineNormScores2014, reranked2014)
-			measure2015 = get_infNDCG(weight, qrelsSampleEval2015, baselineNormScores2015, reranked2015)
+			currVal = get_infNDCG(weight, qrelsSampleEval, baselineNormScores, rerankedResultsFile)
 		else: # measure == "Pn"
-			prec_num = int(measure[1:])
-			measure2014 = get_precision_at(prec_num, weight, utils.QRELS_2014, baselineNormScores2014, reranked2014)
-			measure2015 = get_precision_at(prec_num, weight, utils.QRELS_2015, baselineNormScores2015, reranked2015)
-		avgMeasure = 0.5 * measure2014 + 0.5 * measure2015
-		print "weight = %f\tavg = %f\t2014 = %f\t2015 = %f" % (weight, avgMeasure, measure2014, measure2015)
-		all_res.append((weight, avgMeasure, measure2014, measure2015))
-		if avgMeasure > measure_max:
-			measure_max = avgMeasure
-			measure_max_2014 = measure2014
-			measure_max_2015 = measure2015
-			best_weight = weight
-	return best_weight, measure_max, measure_max_2014, measure_max_2015, all_res
+			precNum = int(measure[1:])
+			simple_rerank(weight, baselineNormScores, rerankedResultsFile)
+			currVal = get_precision_at(precNum, qrelsTrecEval, rerankedResultsFile)
+		maxVal = max(maxVal, currVal)
+		weightMap.append((weight, currVal))
+		print "%f" % (currVal)
+	
+	return maxVal, [weight for (weight, val) in weightMap if val == maxVal]
 
-def lambda_rerank():
-	best_weight, measure_max, measure_max_2014, measure_max_2015, all_res = get_best_weight("P10")
-	for res in all_res:
-		print "weight = %f\tavg = %f\t2014 = %f\t2015 = %f" % (res[0], res[1], res[2], res[3])
-	print "\nBestWeight=%f\tmax avg = %f\tBest2014=%f\tBest2015=%f" % (best_weight, measure_max, measure_max_2014, measure_max_2015)
-	simple_rerank(best_weight, baselineNormScores2014, reranked2014)
-	simple_rerank(best_weight, baselineNormScores2015, reranked2015)
 
-lambda_rerank()
+def lambdaRerank(measure):
+	maxVal, weights = getBestWeights(measure, qrelsTrecEval, qrelsSampleEval, baselineNormScores, rerankedFile)
+	print "\nMax%s=%f" % (measure, maxVal)
+	print "Weights=%.2f %.2f" % (min(weights), max(weights))
+	#pprint.pprint(weights)
+	simple_rerank(weights[0], baselineNormScores, rerankedFile)
 
-#bestAvgWeight = {"diag":0.6, "test":0.9, "treat":0.55}
-#simple_rerank(bestAvgWeight[class_id], baselineNormScores2014, reranked2014)
-#simple_rerank(bestAvgWeight[class_id], baselineNormScores2015, reranked2015)
+def rerankWithWeights():
+	bestAvgWeight = {"diag":0.64, "test":1.0, "treat":0.64}
+	weight = bestAvgWeight[class_id]
+	print "Weights=%.2f %.2f" % (weight, weight)
+	simple_rerank(weight, baselineNormScores, rerankedFile)
+	
+#lambdaRerank("P10")
+rerankWithWeights()
