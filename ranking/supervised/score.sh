e2f9edcaf2754e3540592ea4580346a070ca6d28
Felix Mance <felixmance@gmail.com>
2016-06-02 16:08:46 +0200

    Tweak l2r convenience scripts

--- ranking/supervised/score.sh
+++ ranking/supervised/score.sh
@@ -1,12 +1,16 @@
-java -jar trunk/bin/RankLib.jar -load $1 -rank ../../ir/results/features-$2.txt -score scores.txt -gmax 2 ${@:3}
+java -jar trunk/bin/RankLib.jar -load model/f$1.ca -rank ../../ir/results/features-2015.txt -score scores.txt ${@:3}
 echo "calling python rerank"
-python rerank.py $2
+python rerank.py 2015
 
 echo
 echo "Test"
- java -jar trunk/bin/RankLib.jar -load $1 -test ../../ir/results/features-$2.txt -metric2T P@10 -gmax 2 ${@:3}
+ java -jar trunk/bin/RankLib.jar -load model/f$1.ca -test ../../ir/results/features-2015.txt -metric2T P@10 ${@:3}
 
 echo 
 
 echo "Trec-eval"
-../../eval/trec_eval.9.0/trec_eval  ../../data/qrels/qrels-treceval-$2.txt results-reranked.txt
+../../eval/trec_eval.9.0/trec_eval  ../../data/qrels/qrels-treceval-2015.txt results-reranked.txt
+
+cd ..
+python unjudged.py ../data/qrels/qrels-treceval-2015.txt supervised/results-reranked.txt
+cd -
--- ranking/supervised/train.sh
+++ ranking/supervised/train.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -jar trunk/bin/RankLib.jar -train ../../ir/results/features-$2.txt -ranker $1 -kcv 5 -kcvmd model/ -kcvmn ca -metric2t P@10 -metric2T P@10 -gmax 2 ${@:3}
+java -jar trunk/bin/RankLib.jar -train ../../ir/results/features-$2.txt -validate ../../ir/results/features-$2.txt -save model/f1.ca -ranker $1 -metric2t P@10 -metric2T P@10  ${@:3}
+
+#java -jar trunk/bin/RankLib.jar -train ../../ir/results/features-$2.txt -ranker $1 -kcv 5 -kcvmd model/ -kcvmn ca -metric2t P@10 -metric2T P@10  ${@:3}
