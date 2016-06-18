2d1efef3306908defe831dff5dba3b35d31a4aff
Felix Mance <felixmance@gmail.com>
2016-06-19 00:58:10 +0200

    Refactor simple rerank

--- ranking/simplererank.py
+++ ranking/simplererank.py
@@ -8,23 +8,32 @@ import numpy as np
 import itertools
 import sys
 import pprint
+from collections import defaultdict
 
 sys.path.insert(0, "../utils/")
 import utils
 
-classifier = sys.argv[1]
-class_id = sys.argv[2]
-year = sys.argv[3]
+CLASS_ID = sys.argv[1]
+YEAR = sys.argv[2]
 
-qrelsSampleEval = "../data/qrels/qrels-sampleval-" + year + ".txt"
-qrelsTrecEval = "../data/qrels/qrels-treceval-" + year + ".txt"
-baselineResultsFile = "../ir/results/results-" + year + "-" + class_id + ".txt"
-rerankedFile = baselineResultsFile + ".reranked." + classifier
+QUERY_OFFSETS = {"diag": 1, "test": 11, "treat": 21}
+QUERY_OFFSET = QUERY_OFFSETS[CLASS_ID]
+QUERY_RANGE = range(QUERY_OFFSET, QUERY_OFFSET + 10)
 
-eval_progname = "../eval/trec_eval.9.0/trec_eval"
-perl_progname = "../eval/sample_eval.pl"
+QRELS_FILE = "../data/qrels/qrels-treceval-" + YEAR + ".txt"
+BASELINE_RESULTS_FILE = "../ir/results/results-" + YEAR + ".txt"
+EVAL_PROGNAME = "../eval/trec_eval.9.0/trec_eval"
 
-topic_offsets = {"diag": 1, "test": 11, "treat": 21}
+CLASSIFIERS = [
+	"LinearSVC.squared_hinge.l2", 
+	"SGDClassifier.hinge.l2",
+	"SGDClassifier.hinge.elasticnet",
+	"SGDClassifier.squared_loss.l2",
+	"SGDClassifier.squared_loss.elasticnet",
+	"SGDClassifier.epsilon_insensitive.l2",
+	"SGDClassifier.epsilon_insensitive.elasticnet",
+	"Pipeline.epsilon_insensitive.l2"
+]
 
 def interpolate(x, y, w):
 	if y == None:
@@ -32,80 +41,82 @@ def interpolate(x, y, w):
 	else:
 		return w * x + (1 - w) * y
 
-def zscore_dict(d):
+def zscoreDictValues(d):
 	return dict(zip(d.keys(), stats.zscore(d.values())))
 
-class_predictions = utils.readClassPredictions(classifier, class_id)
-class_predictions = zscore_dict(class_predictions)
-baselineResults = utils.readResults(baselineResultsFile)
-
-topic_offset = topic_offsets[class_id]
-
-def getNormScores(baselineResults):
+def getBaselineScores(baselineResultsFile):
+	baselineResults = utils.readResults(baselineResultsFile)
 	normScores = {}
-	for topic_id in range(topic_offset, topic_offset + 10):
-		doc, _, scores = zip(*(baselineResults[topic_id]))
-		normScores[topic_id] = dict(zip(doc, stats.zscore(scores)))
+	for qid in QUERY_RANGE:
+		doc, _, scores = zip(*(baselineResults[qid]))
+		normScores[qid] = dict(zip(doc, stats.zscore(scores)))
 	return normScores
 
-baselineNormScores = getNormScores(baselineResults)
+def getClassifierScores():
+	scores = defaultdict(float)
+	for classifier in CLASSIFIERS:
+		for did, score in utils.readClassPredictions(classifier, CLASS_ID).items():
+			scores[did] += score/len(CLASSIFIERS)
+	return zscoreDictValues(scores)
 
-def simple_rerank(weight, baselineNormScores, rerankedFile):
-	reranked_results = open(rerankedFile, "w")
-	for topic_id in range(topic_offset, topic_offset + 10):
-		norm_scores = baselineNormScores[topic_id]
-		reranked = [(doc_id, interpolate(norm_scores[doc_id], class_predictions.get(doc_id), weight)) for doc_id in norm_scores.keys()]
+def getP10(qrelsFile, resultsFile):
+	output = subprocess.Popen([EVAL_PROGNAME, qrelsFile, resultsFile], stdout=subprocess.PIPE).communicate()[0]
+	output = output.split()
+	index = output.index("P_10")
+	return float(output[index + 2])
 
-		reranked.sort(key = lambda doc_score : doc_score[1], reverse = True)
+def rerankScores(weight, baselineScores, classifierScores, rerankedFile):
+	out = open(rerankedFile, "w")
+	for qid in QUERY_RANGE:
+		queryScores = baselineScores[qid]
+		rerankedScores = [(did, interpolate(queryScores[did], classifierScores.get(did), weight))
+						   for did in queryScores.keys()]
+		rerankedScores.sort(key = lambda docScore : docScore[1], reverse = True)
 		rank = 1
-		for doc_id, score in reranked:
-			reranked_results.write("%d Q0 %s %d %f STANDARD\n" % (topic_id, doc_id, rank, score))
+		for did, score in rerankedScores:
+			out.write("%d Q0 %s %d %f STANDARD\n" % (qid, did, rank, score))
 			rank += 1
-	reranked_results.close()
+	out.close()
 
-def get_precision_at(prec_num, qrelsFile, rerankedFile):
-	output = subprocess.Popen([eval_progname, qrelsFile, rerankedFile], stdout=subprocess.PIPE).communicate()[0]
-	output = output.split()
-	index = output.index("P_" + str(prec_num))
-	return float(output[index + 2])
-
-def get_infNDCG(weight, qrelsSampleEvalFile, baselineNormScores, rerankedFile):
-	simple_rerank(weight, baselineNormScores, rerankedFile)
-	output = subprocess.Popen(["perl", perl_progname, qrelsSampleEvalFile, rerankedFile], stdout=subprocess.PIPE).communicate()[0]
-	output = output.split()
-	index = output.index("infNDCG")
-	return float(output[index + 2])
-
-def getBestWeights(measure, qrelsTrecEval, qrelsSampleEval, baselineNormScores, rerankedResultsFile):
-	weightMap = []
-	maxVal = 0.0
+def lambdaRerank(qrelsFile, baselineScores, classifierScores, rerankedFile):
+	allP10 = []
+	maxP10 = 0.0
 	for weight in np.linspace(0.2,1,41):
-		if measure == "infNDCG":
-			currVal = get_infNDCG(weight, qrelsSampleEval, baselineNormScores, rerankedResultsFile)
-		else: # measure == "Pn"
-			precNum = int(measure[1:])
-			simple_rerank(weight, baselineNormScores, rerankedResultsFile)
-			currVal = get_precision_at(precNum, qrelsTrecEval, rerankedResultsFile)
-		maxVal = max(maxVal, currVal)
-		weightMap.append((weight, currVal))
-		print "%f" % (currVal),
+		rerankScores(weight, baselineScores, classifierScores, rerankedFile)
+		p10 = getP10(qrelsFile, rerankedFile)
+		maxP10 = max(p10, maxP10)
+		allP10.append((weight, p10))
+		print "%f" % p10
 	
-	return maxVal, [weight for (weight, val) in weightMap if val == maxVal], weightMap
+	bestWeights = [weight for (weight, p10) in allP10 if p10 == maxP10]
+	print "MaxP10=%f" % maxP10,
+
+	rerankScores(bestWeights[0], baselineScores, classifierScores, rerankedFile)
+	return maxP10, allP10
+
+def run():
+	baselineScores = getBaselineScores(BASELINE_RESULTS_FILE)
+	classifierScores = getClassifierScores()
+	rerankedFile = BASELINE_RESULTS_FILE + ".reranked." + CLASS_ID
+ 	lambdaRerank(QRELS_FILE, baselineScores, classifierScores, rerankedFile)
+
+run()
+
+#def random(weight):
+#	s=0.0
+#	for i in range(1,101):
+#		global class_predictions
+#		class_predictions = utils.readclassifierScores(classifier, CLASS_ID)
+#		class_predictions = zscore_dict(class_predictions)
+#	
+#	#	getBestWeights("P10", qrelsTrecEval, qrelsSampleEval, baselineNormScores, rerankedFile+".random")
+#		simple_rerank(weight, baselineNormScores, rerankedFile+".random")
+#		p10 = get_precision_at("10", qrelsTrecEval, rerankedFile+".random")
+#		s+=p10
+#		print "%f %f" % (p10, s/i)
 
 
-def lambdaRerank(measure):
-	maxVal, weights, weightMap = getBestWeights(measure, qrelsTrecEval, qrelsSampleEval, baselineNormScores, rerankedFile)
-	print "Max%s=%f" % (measure, maxVal),
-	print "Weights=%.2f-%.2f" % (min(weights), max(weights)),
-#	pprint.pprint(weights)
-	simple_rerank(weights[0], baselineNormScores, rerankedFile)
-	return weightMap
+#lambdaRerank("P10")
+#random(0.75)
 
-def rerankWithWeights():
-	bestAvgWeight = {"diag":0.64, "test":1.0, "treat":0.64}
-	weight = bestAvgWeight[class_id]
-	print "Weights=%.2f %.2f" % (weight, weight)
-	simple_rerank(weight, baselineNormScores, rerankedFile)
-	
-lambdaRerank("P10")
-#rerankWithWeights()
+
